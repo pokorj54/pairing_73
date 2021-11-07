8c99f2a1eaac3ddaa3c441679d8a361d157dcc39
Jan Pokorný <pokorj54@fit.cvut.cz>
2021-11-07 02:09:05 +0100

    python checker

--- src/checker.py
+++ src/checker.py
@@ -10,19 +10,39 @@ def get_all_points():
     return res
 
 def check_play(points):
-    subprocess.run(['ls', '-l'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    proc_board = subprocess.run(["./exe/determine_board_pc", *points], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    board = proc_board.stdout.decode('utf-8').strip()
+    if proc_board.returncode != 0:
+        return
+    proc_selector = subprocess.run(["./exe/position_selector", board], stdout=subprocess.PIPE)
+    if proc_selector.returncode != 0:
+        print('selector', proc_selector.stderr.decode('utf-8'), points, board)
+        return
+    byte_solution = proc_selector.stdout
+    proc_verifier = subprocess.Popen(["./exe/verifier"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    proc_verifier.stdin.write(byte_solution)
+    verifier_output = proc_verifier.communicate()[0]
+    proc_verifier.stdin.close()
+
+    if proc_verifier.returncode != 0:
+        print('veifier', verifier_output.decode('utf-8'), points, board)
+        return 
 
 
-all_points = get_all_points()
+def main():
+    all_points = get_all_points()
+
+    for a in all_points:
+        print(a)
+        if(a == '333'):
+            continue
+        for b in all_points:
+            print(a, b)
+            for c in all_points:
+                check_play([a,b,c])
+                check_play(['333',a,b,c])
+    print("done")
 
 
-
-
-for a in all_points:
-    print(a)
-    if(a == '333'):
-        continue
-    for b in all_points:
-        for c in all_points:
-            check_play([a,b,c])
-            check_play(['333',a,b,c])
+if __name__ == '__main__':
+    main()
